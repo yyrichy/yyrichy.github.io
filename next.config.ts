5858979aac3a8983ea0e4bfb5da7206e199d97ca
yyrichy <ryvapor@gmail.com>
2025-03-03 22:53:11 -0500

    export

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export', // Enable static exports
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
